4cfdcd4c1735e302e706f040afa38ef31c61c75e
aganoyot <aganoyot@gmail.com>
2015-09-11 00:38:13 +0900

    監視対象から外す

--- Vimman/app/src/main/java/com/example/himejima/vimman/QuestionActivityFragment.java
+++ Vimman/app/src/main/java/com/example/himejima/vimman/QuestionActivityFragment.java
@@ -1,11 +1,19 @@
 package com.example.himejima.vimman;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -20,4 +28,60 @@ public class QuestionActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_question, container, false);
     }
+
+    /*
+    public class FetchQuestionTask extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchQuestionTask.class.getSimpleName();
+        @Override
+        protected Void doInBackground(Void... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String questionJsonStr = null;
+
+            try {
+                URL url = new URL("http://example");
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                questionJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+
+            }
+
+            return null;
+        }
+    }
+    */
 }
