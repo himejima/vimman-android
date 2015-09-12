264f422259341ceff7fd1e21c502312c770825a4
aganoyot <aganoyot@gmail.com>
2015-09-13 01:36:47 +0900

    結果画面で値を受け渡すところまで実装

--- Vimman/app/src/main/java/com/example/himejima/vimman/ResultActivity.java
+++ Vimman/app/src/main/java/com/example/himejima/vimman/ResultActivity.java
@@ -1,9 +1,14 @@
 package com.example.himejima.vimman;
 
+import java.util.Set;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import android.content.Intent;
+import android.util.Log;
 
 
 public class ResultActivity extends Activity {
@@ -12,6 +17,16 @@ public class ResultActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        Set<String> set = bundle.keySet();
+        for (String entry : set) {
+            Log.d("ss", entry);
+            String val = (String)bundle.get(entry);
+            Log.d("aa", val);
+        }
     }
 
     @Override
